b2286443adb7190528a030f47fe4200a426745d5
rashid.huseynov <rashid.huseynov@idda.az>
2026-09-03 11:53:08 +0400

    Initial Commit

---
+++ Blitz/BlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlitzApp.swift
+//  Blitz
+//
+//  Created by Rashid Huseynov on 01.09.26.
+//
+
+import SwiftUI
+
+@main
+struct BlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
